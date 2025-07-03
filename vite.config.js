81e4c79a98727380740e8a7f17e2637257f35b1c
Dan Munteanu <epsdel@gmail.com>
2025-07-04 00:31:35 +0300

    Updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
