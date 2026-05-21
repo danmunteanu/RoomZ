e01a197e0d9f5c114b3b265d943e3389e8204e16
Dan Munteanu <epsdel@gmail.com>
2026-05-21 23:50:25 +0300

    Refactoring

---
+++ resources/js/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import Canvas from './components/Canvas.vue';
+import Editors from './components/Editors.vue';
+
+</script>
+
+<template>
+  <div class="grid grid-cols-2 mx-40 mt-15">
+      <Editors />
+      <Canvas />
+  </div>
+</template>
+
+<style scoped>
+#canvas {
+  width: 100%;
+  width: 100%;
+  min-height: 480px;
+  display: block;
+}
+</style>
---
+++ resources/js/components/Scene.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import Scene3D from '../classes/scene3D.js';
+
+let scene3D = null;
+
+onMounted(() => {
+  scene3D = new Scene3D("canvas");
+        
+  // Handle window resize
+  window.addEventListener('resize', () => {
+      scene3D.renderer.setSize(scene3D.canvas.clientWidth, scene3D.canvas.clientHeight, false);
+      scene3D.camera.aspect = scene3D.canvas.clientWidth / scene3D.canvas.clientHeight;
+      scene3D.camera.updateProjectionMatrix();
+  });
+});
+</script>
+
+<template>
+	<canvas id="canvas"></canvas>
+</template>
